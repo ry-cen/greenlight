4d76b33ab8b161cbfa4429b186f6561a65a99174
Ryan Cen <rycen618@gmail.com>
2023-07-13 20:21:56 -0700

    wip

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -2,24 +2,47 @@ package main
 
 import (
 	"fmt"
+	"greenlight/internal/data"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
+	"time"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
-}
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
 
-func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	params := httprouter.ParamsFromContext(r.Context())
-
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	err := app.readJSON(w, r, &input)
 	if err != nil {
-		http.NotFound(w, r)
+		app.badRequestResponse(w, r, err)
 		return
 	}
 
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	fmt.Fprintf(w, "%+v\n", input)
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
 }
